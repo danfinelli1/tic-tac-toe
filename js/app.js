786074cd306b4e787ac48a8580991c59a6063292
Dan Finelli <dan.finelli@hotmail.com>
2016-10-28 16:42:23 -0700

    x and o function and reset button

--- js/app.js
+++ js/app.js
@@ -2,15 +2,36 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-  function drawX() {
-    $(this).prepend('<img id="letterX" src="letterX.jpg"/>');
+  var player = [];
+  var turnP1 = true;
+  var turn = 1;
 
-  }
-  $(".box").click(function() {
-    console.log(this);
-    $(this).prepend('<img id="letterX" src="letterX.jpg"/>');
+  player[0] = {
+    name: "Player 1",
+    gamesWon: 0,
+    gamesLost: 0,
+    gamesTied: 0
+  };
 
+  player[1] = {
+    name: "Player 2",
+    gamesWon: 0,
+    gamesLost: 0,
+    gamesTied: 0
+  };
 
-  });
+function newBoard() {
+  $(".box").empty();
+}
+function drawXNO() {
+  if (turn%2===1) {
+          $(this).prepend('<img id="letterX" src="letterX.jpg"/>');
+          turn++;
+} else if(turn%2===0) {
+          $(this).prepend('<img id="letterO" src="letterO.jpg"/>');
+          turn++;}}
+
+$("button").click(newBoard);
+$(".box").click(drawXNO);
   console.log('im ready!');
 });
