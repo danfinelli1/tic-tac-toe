4b069a471948aef9d434cccfde7f52b590a1405c
Dan Finelli <dan.finelli@hotmail.com>
2016-10-28 16:02:25 -0700

    event listener and X done

--- js/app.js
+++ js/app.js
@@ -2,5 +2,15 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  function drawX() {
+    $(this).prepend('<img id="letterX" src="letterX.jpg"/>');
 
+  }
+  $(".box").click(function() {
+    console.log(this);
+    $(this).prepend('<img id="letterX" src="letterX.jpg"/>');
+
+
+  });
+  console.log('im ready!');
 });
